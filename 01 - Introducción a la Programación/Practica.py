eb727f278862ced956a2ab306c38a09652f1b2d6
anasilemospina <anaospina90@hotmail.com>
2023-01-15 19:32:29 -0500

    agregue 2 archvos mas

--- 01 - Introducción a la Programación/Practica.py
+++ 01 - Introducción a la Programación/Practica.py
@@ -1,1 +1,51 @@
-print('Hola Mundo!222')
+numero= 10
+print(numero)
+
+NUMEROCONSTANTE= 8.5
+print(type(NUMEROCONSTANTE))
+
+
+print(type(numero))
+
+nombre = "ana"
+
+c1= 5 + 6j
+
+print(c1)
+print(type(c1))
+
+variable1= True
+variable2= "true"
+print(type(variable1))
+print(type(variable2))
+
+variable3= 10
+variable4= 10.5
+
+print(variable3 + variable4)
+
+c2= (2 + 2j)
+c3 = (3 + 3j )
+print(c2+c3)
+
+numero1 = 2
+numero2= 3
+print(numero1 * numero2)
+
+numero3= 2**8
+print(numero3)
+
+numero4= (27/4)
+print(numero4)
+
+numero5= (27//4)
+print(numero5)
+
+numero6= (27%4)
+print(numero6)
+
+numero7 = 4 * numero5 + numero6
+print(numero7)
+
+
+
